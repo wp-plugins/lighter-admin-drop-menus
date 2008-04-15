bd95878c92f4597000c417cde207a453658021b5
corpodibacco <corpodibacco@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-15 14:29:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/lighter-admin-drop-menus/tags/2.5.9@40753 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lighter-menus.php
+++ lighter-menus.php
@@ -3,7 +3,7 @@
 Plugin Name: Lighter Menus
 Plugin URI: http://www.italyisfalling.com/lighter-admin-drop-menus-wordpress-plugin
 Description: Creates Drop Down Menus for WordPress Admin Panels. Fast to load, adaptable to color schemes, comes with silk icons, a option page,  and a design that fits within the Wordpress 2.5 interface taking the less room possible.
-Version: 2.5.8
+Version: 2.5.9
 Author: corpodibacco
 Author URI: http://www.italyisfalling.com/coding/
 WordPress Version: 2.5
